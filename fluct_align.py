3e4a9862f0f2525a58611650e7883246fe638f07
PIERRE BEDOUCHA <xbyska@fi.muni.cz>
2020-02-02 14:34:19 +0100

    Gaps handling, fix for last gap

--- fluct_align.py
+++ fluct_align.py
@@ -36,6 +36,7 @@ def read_msa_fasta():
                     pdb_align_dict[rec.id[:-4]].append(res_cpt + read_pdb_starts()[rec.id[:-4]])
     return pdb_align_dict
 
+
 def read_msa_fasta_gaps():
     """
     Reads msa from MUSTANG in webnma3. It conserves the nan and puts them in the returned dictionary.
@@ -47,13 +48,14 @@ def read_msa_fasta_gaps():
     tlist = list(zip(*records))
     for i in range(0, records.get_alignment_length()):
         for rec in records:
-                ls = [i for i, e in enumerate(rec.seq)]
-                if not rec.seq[i] == "-":
-                    res_cpt = ls.index(i)
-                    pdb_align_dict[rec.id[:-4]].append(res_cpt + read_pdb_starts()[rec.id[:-4]])
-                else:
-                    pdb_align_dict[rec.id[:-4]].append(np.nan)
+            ls = [i for i, e in enumerate(rec.seq)]
+            if not rec.seq[i] == "-":
+                res_cpt = ls.index(i) + 1
+                pdb_align_dict[rec.id[:-4]].append(res_cpt + read_pdb_starts()[rec.id[:-4]])
+            else:
+                pdb_align_dict[rec.id[:-4]].append(np.nan)
     return pdb_align_dict
+
 
 # Global variables (Ugly)
 ca_align_list = []
@@ -62,8 +64,10 @@ ca_align_dict = read_msa_fasta_gaps()
 if __name__ == '__main__':
 
     resid_list = []
+    resid_list_BEB = []
     fluct_list_BE = []
     fluct_list_B = []
+    fluct_list_BEB = []
     train = pd.DataFrame(columns=['resid', 'fluct_score_BE', 'fluct_score_B'])
 
     with open('alignment_fluctuations_webnma3.txt', 'r') as f1:
@@ -75,6 +79,14 @@ if __name__ == '__main__':
                     fluct_list_B.append(float(line.split("\t")[1].strip().replace(",", ".")))
                     fluct_list_BE.append(float(line.split("\t")[2].strip().replace(",", ".")))
 
+    with open('4kvm_chainsBE_modesv2_fluct.dat', 'r') as f1:
+        for line in f1.readlines():
+            resid = int(line.split("\t")[0].strip())
+            if not line.startswith("index") and not line.startswith("\n"):
+                if math.isnan(ca_align_dict["4kvm_chainsBE"][718 + resid - 1]):
+                    fluct_list_BEB.insert(718 + resid - 1, math.nan)
+                else:
+                    fluct_list_BEB.append(float(line.split("\t")[1].strip().replace(",", ".")))
 
     train['resid'] = resid_list
     train['fluct_score_BE'] = fluct_list_BE
@@ -83,13 +95,15 @@ if __name__ == '__main__':
     # Truncate the dataframe from when fluct_score_B starts
     train = train.truncate(before=train['fluct_score_B'].first_valid_index())
 
-    # min max normalization
-    column_names_to_normalize = ['fluct_score_B', 'fluct_score_BE']
-    x = train[column_names_to_normalize].values
-    min_max_scaler = preprocessing.MinMaxScaler()
-    x_scaled = min_max_scaler.fit_transform(x)
-    df_temp = pd.DataFrame(x_scaled, columns=column_names_to_normalize, index=train.index)
-    train[column_names_to_normalize] = df_temp
+    train['fluct_score_BEB'] = fluct_list_BEB
+
+    ## min max normalization
+    # column_names_to_normalize = ['fluct_score_B', 'fluct_score_BE']
+    # x = train[column_names_to_normalize].values
+    # min_max_scaler = preprocessing.MinMaxScaler()
+    # x_scaled = min_max_scaler.fit_transform(x)
+    # df_temp = pd.DataFrame(x_scaled, columns=column_names_to_normalize, index=train.index)
+    # train[column_names_to_normalize] = df_temp
 
     # train = (train - train.min()) / (train.max() - train.min())
 
@@ -102,7 +116,7 @@ if __name__ == '__main__':
 
     ax = plt.gca()
 
-    train.plot(kind='line', x='resid', y='fluct_score_BE', ax=ax)
+    train.plot(kind='line', x='resid', y='fluct_score_BEB', ax=ax)
     train.plot(kind='line', x='resid', y='fluct_score_B', color='red', ax=ax)
 
     plt.show()
